314e0d9e78c9b3b0d37918beacd85cb4b5463e33
Jem <0x0xjem@gmail.com>
2022-09-29 15:23:28 +0400

    Bump subgraph version

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const DATE_EARLIEST = new Date("2021-11-24T00:00:00.000Z");
-export const DATE_FINAL = new Date("2022-02-28");
+export const DATE_FINAL = new Date();
 export const SUBGRAPH_URL =
-  "https://api.studio.thegraph.com/query/28103/token-holders/0.0.29";
+  "https://api.studio.thegraph.com/query/28103/token-holders/0.0.35";
