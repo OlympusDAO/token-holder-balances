66a8077b70873c2a1d2bbb9e8fce840692c68608
Jem <0x0xjem@gmail.com>
2022-10-26 21:16:45 +0400

    Implemented view for token balances with named contracts

--- index.ts
+++ index.ts
@@ -1,19 +1,16 @@
 import * as gcp from "@pulumi/gcp";
 import * as pulumi from "@pulumi/pulumi";
+import { FileAsset } from "@pulumi/pulumi/asset";
 import { readFileSync } from "fs";
 
 import { handler } from "./src/index";
 
 const pulumiConfig = new pulumi.Config();
+const gcpConfig = new pulumi.Config("gcp");
 
-const RECORDS_BUCKET_NAME = pulumiConfig.get("bucketName");
-if (!RECORDS_BUCKET_NAME) throw new Error("bucketName must be set");
-
-const RECORDS_BUCKET_PREFIX = pulumiConfig.get("bucketPrefix");
-if (!RECORDS_BUCKET_PREFIX) throw new Error("bucketPrefix must be set");
-
-const PUBSUB_TOPIC = pulumiConfig.get("pubSubTopic");
-if (!PUBSUB_TOPIC) throw new Error("pubSubTopic must be set");
+const RECORDS_BUCKET_NAME = pulumiConfig.require("bucketName");
+const RECORDS_BUCKET_PREFIX = pulumiConfig.require("bucketPrefix");
+const PUBSUB_TOPIC = pulumiConfig.require("pubSubTopic");
 
 const BUCKET_NAME_PREFIX = `olympusdao-token-balances-${pulumi.getStack()}`;
 const FUNCTION_PREFIX = `token-balances`;
@@ -23,14 +20,14 @@ const functionName = `${FUNCTION_PREFIX}-${pulumi.getStack()}`;
  * Record storage: GCS bucket
  */
 // Create a bucket to store the cached results
-const storageBucket = new gcp.storage.Bucket(BUCKET_NAME_PREFIX, {
+const tokenBalancesBucket = new gcp.storage.Bucket(BUCKET_NAME_PREFIX, {
   location: "US", // Get this from the provider instead?
   uniformBucketLevelAccess: true,
   versioning: { enabled: false },
 });
 
 // Export the DNS name of the bucket
-export const storageBucketUrl = storageBucket.url;
+export const tokenBalancesBucketUrl = tokenBalancesBucket.url;
 
 /**
  * Execution: Google Cloud Functions
@@ -38,7 +35,7 @@ export const storageBucketUrl = storageBucket.url;
 const functionTimeoutSeconds = 540;
 
 // Create a function
-const tokenHolderFunction = new gcp.cloudfunctions.HttpCallbackFunction(functionName, {
+const tokenBalancesFunction = new gcp.cloudfunctions.HttpCallbackFunction(functionName, {
   runtime: "nodejs14",
   timeout: functionTimeoutSeconds,
   availableMemoryMb: 1024,
@@ -47,7 +44,7 @@ const tokenHolderFunction = new gcp.cloudfunctions.HttpCallbackFunction(function
     console.log("Received callback. Initiating handler.");
     await handler(
       FUNCTION_PREFIX,
-      storageBucket.name.get(),
+      tokenBalancesBucket.name.get(),
       RECORDS_BUCKET_PREFIX,
       RECORDS_BUCKET_NAME,
       functionTimeoutSeconds,
@@ -60,7 +57,7 @@ const tokenHolderFunction = new gcp.cloudfunctions.HttpCallbackFunction(function
   },
 });
 
-export const functionUrl = tokenHolderFunction.httpsTriggerUrl;
+export const tokenBalancesFunctionUrl = tokenBalancesFunction.httpsTriggerUrl;
 
 // TODO set max instances
 
@@ -70,33 +67,45 @@ export const functionUrl = tokenHolderFunction.httpsTriggerUrl;
 const pubSubDeadLetter = new gcp.pubsub.Topic(`${PUBSUB_TOPIC}-deadLetter`, {});
 
 const expirationSeconds = 24 * 60 * 60;
-const pubSubSubscription = new gcp.pubsub.Subscription(functionName, {
-  topic: PUBSUB_TOPIC,
-  pushConfig: {
-    pushEndpoint: functionUrl,
+const pubSubSubscription = new gcp.pubsub.Subscription(
+  functionName,
+  {
+    topic: PUBSUB_TOPIC,
+    pushConfig: {
+      pushEndpoint: tokenBalancesFunctionUrl,
+    },
+    // If there is an issue, give up quickly, as it will be picked up later
+    deadLetterPolicy: {
+      deadLetterTopic: pubSubDeadLetter.id,
+      maxDeliveryAttempts: 5, // Lowest possible value
+    },
+    expirationPolicy: { ttl: `${expirationSeconds}s` },
+    messageRetentionDuration: `${expirationSeconds}s`,
   },
-  // If there is an issue, give up quickly, as it will be picked up later
-  deadLetterPolicy: {
-    deadLetterTopic: pubSubDeadLetter.id,
-    maxDeliveryAttempts: 5, // Lowest possible value
+  {
+    dependsOn: [tokenBalancesFunction],
   },
-  expirationPolicy: { ttl: `${expirationSeconds}s` },
-  messageRetentionDuration: `${expirationSeconds}s`,
-});
+);
 
 export const pubSubSubscriptionName = pubSubSubscription.name;
 
 /**
  * Scheduling: Cloud Scheduler
  */
-const schedulerJob = new gcp.cloudscheduler.Job(functionName, {
-  schedule: "10 * * * *", // Start of every hour
-  timeZone: "UTC",
-  httpTarget: {
-    httpMethod: "GET",
-    uri: functionUrl,
+const schedulerJob = new gcp.cloudscheduler.Job(
+  functionName,
+  {
+    schedule: "10 * * * *", // Start of every hour
+    timeZone: "UTC",
+    httpTarget: {
+      httpMethod: "GET",
+      uri: tokenBalancesFunctionUrl,
+    },
   },
-});
+  {
+    dependsOn: tokenBalancesFunction,
+  },
+);
 
 export const schedulerJobName = schedulerJob.name;
 
@@ -105,11 +114,15 @@ export const schedulerJobName = schedulerJob.name;
  *
  * We do this, otherwise the Hive partitioning will complain of no files being present.
  */
-const dummyObject = new gcp.storage.BucketObject("dummy", {
-  bucket: storageBucket.name,
-  content: "{}", // Empty file
-  name: `${FUNCTION_PREFIX}/dt=2021-01-01/dummy.jsonl`,
-});
+const dummyObject = new gcp.storage.BucketObject(
+  "dummy",
+  {
+    bucket: tokenBalancesBucket.name,
+    content: "{}", // Empty file
+    name: `${FUNCTION_PREFIX}/dt=2021-01-01/dummy.jsonl`,
+  },
+  { dependsOn: [tokenBalancesBucket] },
+);
 
 /**
  * Create a BigQuery external table for balance records
@@ -120,9 +133,10 @@ const bigQueryDataset = new gcp.bigquery.Dataset(BUCKET_NAME_PREFIX, {
 
 export const bigQueryDatasetId = bigQueryDataset.datasetId;
 
-// storageBucketUrl is not known until deploy-time, so we use a pulumi-provided function to utilise it
+// tokenBalancesBucketUrl is not known until deploy-time, so we use a pulumi-provided function to utilise it
 // Source: https://www.pulumi.com/docs/intro/concepts/inputs-outputs/#apply
-const tokenBalancesSourceUri = storageBucketUrl.apply(url => `${url}/${FUNCTION_PREFIX}/*`);
+const tokenBalancesSourceUriPrefix = tokenBalancesBucketUrl.apply(url => `${url}/${FUNCTION_PREFIX}/`);
+const tokenBalancesSourceUri = tokenBalancesBucketUrl.apply(url => `${url}/${FUNCTION_PREFIX}/*`);
 
 // For the moment, we generate a BigQuery schema file and store it locally
 const tokenBalancesSchema = readFileSync("schema-token-balances.json").toString("utf-8");
@@ -138,13 +152,13 @@ const tokenBalancesTable = new gcp.bigquery.Table(
       sourceUris: [tokenBalancesSourceUri],
       hivePartitioningOptions: {
         mode: "AUTO",
-        sourceUriPrefix: tokenBalancesSourceUri,
+        sourceUriPrefix: tokenBalancesSourceUriPrefix,
       },
       autodetect: false,
       schema: tokenBalancesSchema,
     },
   },
-  { dependsOn: dummyObject },
+  { dependsOn: [dummyObject, tokenBalancesBucket] },
 );
 
 export const tokenBalancesTableId = tokenBalancesTable.tableId;
@@ -153,13 +167,18 @@ export const tokenBalancesTableId = tokenBalancesTable.tableId;
  * Upload the contract addresses to the GCS bucket
  */
 const contractAddressesBucketPrefix = "contract-addresses";
-const contractAddressesObject = new gcp.storage.BucketObject("contract-addresses", {
-  bucket: storageBucket.name,
-  content: readFileSync("addresses.csv").toString(),
-  name: `${contractAddressesBucketPrefix}/addresses.csv`,
-});
-
-const contractAddressesSourceUri = storageBucketUrl.apply(url => `${url}/${contractAddressesBucketPrefix}/*`);
+const contractAddressesObject = new gcp.storage.BucketObject(
+  "contract-addresses",
+  {
+    bucket: tokenBalancesBucket.name,
+    source: new FileAsset("addresses.csv"),
+    name: `${contractAddressesBucketPrefix}/addresses.csv`,
+  },
+  {
+    dependsOn: [tokenBalancesBucket],
+  },
+);
+const contractAddressesSourceUri = tokenBalancesBucketUrl.apply(url => `${url}/${contractAddressesBucketPrefix}/*`);
 
 /**
  * Create a BigQuery external table for contract names
@@ -189,23 +208,28 @@ const contractAddressesTable = new gcp.bigquery.Table(
 export const contractAddressesTableId = contractAddressesTable.tableId;
 
 /**
- * Create a BigQuery materialized view with the token balances and contract names
+ * Create a BigQuery view with the token balances and contract names.
+ *
+ * A materialised view (persisted) would be ideal, but external tables are not supported.
  */
+const tokenBalancesNamedQuery = pulumi.interpolate`
+  SELECT
+  balances.*,
+  IF(addresses.Custom_Name, addresses.Name, balances.holder) AS holder_name,
+  IFNULL(addresses.Type, "Unknown") AS holder_type,
+  FROM \`${gcpConfig.require("project")}.${bigQueryDatasetId}.${tokenBalancesTableId}\` as balances
+  LEFT JOIN \`${bigQueryDatasetId}.${contractAddressesTableId}\` as addresses ON addresses.Address = balances.holder
+  `;
+
 const tokenBalancesNamedTable = new gcp.bigquery.Table(
   "token-balances-named",
   {
     datasetId: bigQueryDatasetId,
     tableId: "token-balances-named",
     deletionProtection: false,
-    materializedView: {
-      query: `
-        SELECT 
-        balances.*, 
-        IF(addresses.Custom_Name, addresses.Name, balances.holder) AS holder_name,
-        IFNULL(addresses.Type, "Unknown") AS holder_type,
-        FROM \`${pulumi.getProject()}.${bigQueryDatasetId}.${tokenBalancesTableId}\` as balances
-        LEFT JOIN \`${bigQueryDatasetId}.${contractAddressesTableId}\` as addresses ON addresses.Address = balances.holder
-      `,
+    view: {
+      query: tokenBalancesNamedQuery,
+      useLegacySql: false,
     },
   },
   { dependsOn: [contractAddressesTable, tokenBalancesTable] },
