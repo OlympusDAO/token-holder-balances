cd3e86c67850867e871286ce7a7e81a866815c3b
Jem <0x0xjem@gmail.com>
2022-10-28 16:32:41 +0400

    Function cleanup

--- src/helpers/recordFs.ts
+++ src/helpers/recordFs.ts
@@ -36,28 +36,55 @@ export const readRecords = async (
   return JSONL.parse((await file.download())[0].toString("utf-8")) as TokenHolderTransaction[];
 };
 
-export const getLatestRecordsDate = async (bucket: string, path: string): Promise<Date | null> => {
+/**
+ * Returns the dates present in the specified GCS bucket.
+ *
+ * This function will check the contents of {bucket}/{path} for files in the format: {bucket}/{path}/dt=YYYY-MM-DD/records.jsonl
+ *
+ * @param bucket name of the GCS bucket
+ * @param path path/prefix under which the partition directories are present
+ * @returns Array of Date objects, in descending order
+ */
+const getRecordDates = async (bucket: string, path: string): Promise<Date[]> => {
   const fileNames = await listFiles(bucket, path);
   if (fileNames.length === 0) {
-    return null;
+    return [];
   }
 
   // Excludes the dummy file
   const recordsFileNames = fileNames.filter(fileName => fileName.includes("records.jsonl"));
   const fileDates = recordsFileNames.map(fileName => new Date(extractPartitionKey(fileName)));
-  const sortedFileDates = fileDates.sort((a, b) => b.getTime() - a.getTime());
+  return fileDates.sort((a, b) => b.getTime() - a.getTime());
+};
+
+/**
+ * Returns the latest date for which a record file exists.
+ *
+ * @param bucket name of the GCS bucket
+ * @param path path/prefix under which the partition directories are present
+ * @returns Date of the latest records file, or null
+ */
+export const getLatestRecordsDate = async (bucket: string, path: string): Promise<Date | null> => {
+  const sortedFileDates: Date[] = await getRecordDates(bucket, path);
+  if (sortedFileDates.length === 0) {
+    return null;
+  }
+
   return sortedFileDates[0];
 };
 
+/**
+ * Returns the earliest date for which a record file exists.
+ *
+ * @param bucket name of the GCS bucket
+ * @param path path/prefix under which the partition directories are present
+ * @returns Date of the earliest records file, or null
+ */
 export const getEarliestRecordsDate = async (bucket: string, path: string): Promise<Date | null> => {
-  const fileNames = await listFiles(bucket, path);
-  if (fileNames.length === 0) {
+  const sortedFileDates: Date[] = await getRecordDates(bucket, path);
+  if (sortedFileDates.length === 0) {
     return null;
   }
 
-  // Excludes the dummy file
-  const recordsFileNames = fileNames.filter(fileName => fileName.includes("records.jsonl"));
-  const fileDates = recordsFileNames.map(fileName => new Date(extractPartitionKey(fileName)));
-  const sortedFileDates = fileDates.sort((a, b) => b.getTime() - a.getTime());
   return sortedFileDates[sortedFileDates.length - 1];
 };
